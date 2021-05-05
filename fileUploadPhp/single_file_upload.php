5f6c4529bb2bcd19d8941fee9eeadfea3100d89c
Tarun Mahajan <2922986+tango4567@users.noreply.github.com>
2021-05-05 15:42:37 +0530

    Single File Upload

---
+++ fileUploadPhp/single_file_upload.php
@@ -0,0 +1,86 @@
+<?php
+define("REPOSITORY", $_SERVER['DOCUMENT_ROOT']);
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+echo REPOSITORY . ' <---------Test<br>';
+
+/**
+ * Description of Multifile Upload
+ *
+ * @author tango4567
+ */
+
+$currentDirectory = getcwd();
+echo 'Current Path: ' . $currentDirectory . '<br>';
+
+$uploadDirectory = "/uploadedFilesStoreHere/";
+
+$errors = []; //-->>errors prints
+
+$fileExtensionsAllowed = ['jpeg', 'jpg', 'png', 'bmp', 'gif']; //-->> file extensions 
+
+$file = $_FILES['file']['name'];
+echo "<br>File: " . $file . "<br>";
+
+$fileError = $_FILES["file"]["error"];
+echo '<br>File Error: ' . $fileError . '<br>';
+
+$fileTmp = $_FILES['file']['name'];
+echo '<br>File Tmp: ' . $fileTmp . '<br>';
+
+$fileName = str_replace(" ", "_", $fileTmp);
+echo '<br>File Name: ' . $fileName . '<br>';
+
+$fileSize = $_FILES['file']['size'];
+echo '<br>File Size: ' . $fileSize . '<br>';
+
+$fileTmpName  = $_FILES['file']['tmp_name'];
+echo '<br>File TmpName: ' . $fileTmpName . '<br>';
+
+$fileType = $_FILES['file']['type'];
+echo '<br>File Type: ' . $fileType . '<br>';
+
+$tmp = pathinfo($fileName, PATHINFO_EXTENSION);
+echo "<br>Tmp: " . $tmp;
+
+$explode = explode('.', $fileName);
+$fileExtension = strtolower(end($explode));
+echo '<br>File Extension: ' . $fileExtension . '<br>';
+
+$uploadPath = $currentDirectory . $uploadDirectory . basename($fileName);
+echo '<br>File upload path: ' . $uploadPath . '<br>';
+
+if (!in_array($fileExtension, $fileExtensionsAllowed)) {
+    $errors[] = "<br>This file extension is not allowed. Please upload a JPEG, bmp,or PNG file.<br>";
+}
+
+if ($fileSize > 4000000) {
+    $errors[] = "<br>File exceeds maximum size (4MB)<br>";
+}
+
+if (!is_writeable(REPOSITORY)) {
+    echo "<br>Cannot write to destination file " . REPOSITORY . "<br>";
+} else {
+    echo "<br>You have write permission<br>";
+}
+
+if (empty($errors)) {
+    echo "<br>---------->" . basename($fileName) . "<br> " . dirname(__FILE__) . "<br>";
+    $didUpload = move_uploaded_file($fileTmpName, $uploadPath);
+
+    echo '<br>------------------------------->' . $didUpload . '<-------------------------------<br>';
+    echo '<br>----------' . $uploadPath . '<br>';
+
+    if ($didUpload == 1) {
+        echo "<br>The file " . basename(REPOSITORY) . " has been uploaded<br>";
+    } else {
+        echo "<br>".basename(REPOSITORY)." An error occurred. Please contact the administrator.<br>";
+    }
+} else {
+    foreach ($errors as $errors) {
+        echo $errors . "<br>These are the errors" . "\n";
+    }
+}
+
